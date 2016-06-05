83158bbbfdd2ab9b1445e5bccb182815d051dae0
Michał Dobrzański <dobrzanski.michal.daniel@gmail.com>
2016-06-05 20:28:48 +0200

    Fill Enums

---
+++ postgres/fillEnums.sql
@@ -0,0 +1,55 @@
+-- Wypełnienie enumów:
+
+-- Typy_pokrewienstwa
+INSERT INTO Typy_pokrewienstwa VALUES ('Kuzyn');
+INSERT INTO Typy_pokrewienstwa VALUES ('Brat');
+INSERT INTO Typy_pokrewienstwa VALUES ('Siostra');
+INSERT INTO Typy_pokrewienstwa VALUES ('Matka');
+INSERT INTO Typy_pokrewienstwa VALUES ('Ojciec');
+INSERT INTO Typy_pokrewienstwa VALUES ('Wujek');
+INSERT INTO Typy_pokrewienstwa VALUES ('Ciotka');
+INSERT INTO Typy_pokrewienstwa VALUES ('Dziadek');
+INSERT INTO Typy_pokrewienstwa VALUES ('Babcia');
+INSERT INTO Typy_pokrewienstwa VALUES ('Wnuczek');
+INSERT INTO Typy_pokrewienstwa VALUES ('Wnuczka');
+INSERT INTO Typy_pokrewienstwa VALUES ('Syn');
+INSERT INTO Typy_pokrewienstwa VALUES ('Corka');
+INSERT INTO Typy_pokrewienstwa VALUES ('Siostrzeniec');
+
+-- Stany_cywilne
+INSERT INTO Stany_cywilne VALUES ('Kawaler');
+INSERT INTO Stany_cywilne VALUES ('Panna');
+INSERT INTO Stany_cywilne VALUES ('Rozwodnik');
+INSERT INTO Stany_cywilne VAL;UES ('Rozwodka');
+INSERT INTO Stany_cywilne VALUES ('Wdowiec');
+INSERT INTO Stany_cywilne VALUES ('Wdowa');
+INSERT INTO Stany_cywilne VALUES ('Maz');
+INSERT INTO Stany_cywilne VALUES ('Zona');
+
+-- Rodzaje_pracownikow
+INSERT INTO Rodzaje_pracownikow VALUES ('Lekarz');
+INSERT INTO Rodzaje_pracownikow VALUES ('Lekarz rodzinny');
+INSERT INTO Rodzaje_pracownikow VALUES ('Lekarz specjalista');
+INSERT INTO Rodzaje_pracownikow VALUES ('Dyrektor');
+INSERT INTO Rodzaje_pracownikow VALUES ('Dyrektor medyczny');
+INSERT INTO Rodzaje_pracownikow VALUES ('Dyrektor personalny');
+INSERT INTO Rodzaje_pracownikow VALUES ('Pielegniarka');
+INSERT INTO Rodzaje_pracownikow VALUES ('Pielegniarka oddzialowa');
+INSERT INTO Rodzaje_pracownikow VALUES ('Sprzatacz');
+INSERT INTO Rodzaje_pracownikow VALUES ('Sprzataczka');
+INSERT INTO Rodzaje_pracownikow VALUES ('Dostawca');
+INSERT INTO Rodzaje_pracownikow VALUES ('Ochroniarz');
+INSERT INTO Rodzaje_pracownikow VALUES ('Recepcjonista');
+INSERT INTO Rodzaje_pracownikow VALUES ('Recepcjolistka');
+INSERT INTO Rodzaje_pracownikow VALUES ('Kierownik');
+INSERT INTO Rodzaje_pracownikow VALUES ('Zarzadca');
+INSERT INTO Rodzaje_pracownikow VALUES ('Informatyk');
+
+-- Rodzaje_umowy 
+INSERT INTO Rodzaje_umowy VALUES ('Umowa na czas okreslony');
+INSERT INTO Rodzaje_umowy VALUES ('Umowa na czas nieokreslony');
+INSERT INTO Rodzaje_umowy VALUES ('Umowa na okres probny');
+INSERT INTO Rodzaje_umowy VALUES ('Umowa zlecenie');
+INSERT INTO Rodzaje_umowy VALUES ('Umowa o dzielo');
+INSERT INTO Rodzaje_umowy VALUES ('Umowa agencyjna');
+
