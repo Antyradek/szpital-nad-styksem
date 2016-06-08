40f7ae68bf664ee051f49068b19661b40da6d728
Marek Majde <mmajde@kontomierz.pl>
2016-06-08 23:30:19 +0200

    poprawka pobierania oddzialu

--- postgres/transactions/przeniesPacjentaNaOddzial.sql
+++ postgres/transactions/przeniesPacjentaNaOddzial.sql
@@ -1,10 +1,10 @@
-CREATE FUNCTION przeniesPacjentaNaOddzial(in int) RETURNS integer AS 
+CREATE FUNCTION przeniesPacjentaNaOddzial(in int, in int) RETURNS integer AS 
 $body$
 BEGIN
 
 DELETE FROM Pacjenci_oczekujacy as p WHERE p.Pacjent_Numer = $1;
 
-INSERT INTO Hospitalizacje VALUES(2000, '2016-06-08', 90, null, $1, (SELECT l.Numer FROM Lozka as l WHERE l.Numer NOT IN (SELECT h.Lozko_Numer FROM Hospitalizacje as h) LIMIT 1));
+INSERT INTO Hospitalizacje VALUES($2, '2016-06-08', 90, null, $1, (SELECT l.Numer FROM Lozka as l WHERE l.Numer NOT IN (SELECT h.Lozko_Numer FROM Hospitalizacje as h) LIMIT 1));
 
 RETURN 0;
 
