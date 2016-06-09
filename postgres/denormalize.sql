c675ad58499d82e6b3bd966b04dfbc02bc1477ee
Antyradek <antyradek@gmail.com>
2016-06-09 03:22:36 +0200

    Denormalizuj podawane leki

--- postgres/denormalize.sql
+++ postgres/denormalize.sql
@@ -47,3 +47,27 @@ CREATE TRIGGER denorm_pacjenci_oczekujacy_trig
 BEFORE INSERT OR UPDATE ON Pacjenci_Oczekujacy
 FOR EACH ROW
 EXECUTE PROCEDURE denorm_pacjenci_oczekujacy();
+
+-- PRE-JOIN Informacje o pacjencie i nazwa leku do dawkowania
+ALTER TABLE Dozowania_Leku
+	ADD COLUMN Pacjent_Imie varchar(50),
+	ADD COLUMN Pacjent_Nazwisko varchar(50),
+	ADD COLUMN Materialy_Nazwa varchar(400),
+	ADD COLUMN Materialy_Opis varchar(3000);
+	
+CREATE OR REPLACE FUNCTION denorm_dozowania_leku() RETURNS TRIGGER AS
+$denorm_dozowania_leku$
+BEGIN
+	SELECT Osoby.Imie,Osoby.Nazwisko INTO NEW.Pacjent_Imie,NEW.Pacjent_Nazwisko 
+		FROM Osoby WHERE NEW.Pacjent_numer = Osoby.Numer;
+	SELECT Materialy.Nazwa,Materialy.Opis INTO NEW.Materialy_Nazwa,NEW.Materialy_Opis
+		FROM Materialy WHERE NEW.Lek_Numer = Materialy.Numer;
+	RETURN NEW;
+END;
+$denorm_dozowania_leku$
+LANGUAGE plpgsql;
+
+CREATE TRIGGER denorm_dozowania_leku_trig
+BEFORE INSERT OR UPDATE ON Dozowania_Leku
+FOR EACH ROW
+EXECUTE PROCEDURE denorm_dozowania_leku();
